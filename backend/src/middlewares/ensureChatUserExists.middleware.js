782596f083d6e6e4ba1759c60d3bf65731ff2f65
samyak-16 <samexecodes@gmail.com>
2025-07-18 01:16:51 +0545

    Added and implemented these middlewares

---
+++ backend/src/middlewares/ensureChatUserExists.middleware.js
@@ -0,0 +1,29 @@
+// ensureChatUserExists.js
+import { ChatUser } from '../models/chatUser.model.js';
+import { ApiError } from '../utils/api-error.js';
+
+const ensureChatUserExists = async (req, res, next) => {
+  const userId = req.user?.userId; // From verified JWT
+  if (!userId) {
+    return res.status(400).json(new ApiError(400, 'User ID is required'));
+  }
+
+  try {
+    const existing = await ChatUser.findOne({ userId });
+
+    if (!existing) {
+      // First interaction → create default ChatUser
+      await ChatUser.create({ userId });
+      console.log('New User Created in  chatuser db ');
+    }
+
+    next(); // Proceed to next middleware or controller
+  } catch (error) {
+    console.error('Error in ensureChatUserExists:', error);
+    res
+      .status(500)
+      .json(new ApiError(500, 'Internal Server Error at ensureChatUserExists'));
+  }
+};
+
+export { ensureChatUserExists };
