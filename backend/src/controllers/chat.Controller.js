8f139577d09555fe582d425fea43f31ea8e1c8a7
samyak-16 <samexecodes@gmail.com>
2025-07-19 02:48:41 +0545

    Added startOrGetPrivateChat controller

--- backend/src/controllers/chat.Controller.js
+++ backend/src/controllers/chat.Controller.js
@@ -1,9 +1,68 @@
 // Chat Controller Outline
 
+import { Chat } from '../models/chat.model.js';
+import { ChatUser } from '../models/chatUser.model.js';
+import { ApiError } from '../utils/api-error.js';
+import { ApiResponse } from '../utils/api-response.js';
+
 // -------------------- SINGLE & GROUP CHAT BASE ACTIONS --------------------
 
 // Start or get 1-on-1 chat
-const startOrGetPrivateChat = async (req, res) => {};
+const startOrGetPrivateChat = async (req, res) => {
+  const userId = req.user?.userId;
+  const participantId = req.body?.participantId; //
+
+  if (!participantId) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "participant's Id is required"));
+  }
+  if (!userId) {
+    return res.status(400).json(new ApiError(400, 'User ID is required'));
+  }
+  if (userId === participantId) {
+    return res
+      .status(400)
+      .json(new ApiError(400, 'Cannot create chat with yourself'));
+  }
+
+  try {
+    const participantUser = await ChatUser.findOne({ userId: participantId });
+    if (!participantUser) {
+      return res
+        .status(404)
+        .json(new ApiError(404, 'participant User not found'));
+    }
+
+    let chat = await Chat.findOne({
+      isGroup: false,
+      participants: { $all: [userId, participantId], $size: 2 },
+    });
+
+    //$all -> $all checks if an array field contains all specified values — in any order.
+    //$size -> $size checks if the array length matches a specific number.
+
+    if (!chat) {
+      console.log('Creating a new Chat');
+
+      chat = await Chat.create({
+        isGroup: false,
+        participants: [userId, participantId],
+      });
+    }
+
+    res
+      .status(200)
+      .json(new ApiResponse(200, chat, 'Private Chat fetched Succesfully'));
+  } catch (error) {
+    console.error('Error starting chat:', error);
+    return res
+      .status(500)
+      .json(
+        new ApiError(500, 'Internal Server Error at startOrGetPrivateChat')
+      );
+  }
+};
 
 // Create new group chat
 const createGroupChat = async (req, res) => {};
