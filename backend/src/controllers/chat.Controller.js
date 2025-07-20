04b55a98ca0f4a6bc3240f614561685bf678fbcc
samyak-16 <samexecodes@gmail.com>
2025-07-20 22:49:54 +0545

    Added createGroupChat feature (Controller)

--- backend/src/controllers/chat.Controller.js
+++ backend/src/controllers/chat.Controller.js
@@ -4,6 +4,7 @@ import { Chat } from '../models/chat.model.js';
 import { ChatUser } from '../models/chatUser.model.js';
 import { ApiError } from '../utils/api-error.js';
 import { ApiResponse } from '../utils/api-response.js';
+import { filterValidUserIds } from '../utils/filterValidUserIds.js';
 
 // -------------------- SINGLE & GROUP CHAT BASE ACTIONS --------------------
 
@@ -65,7 +66,58 @@ const startOrGetPrivateChat = async (req, res) => {
 };
 
 // Create new group chat
-const createGroupChat = async (req, res) => {};
+const createGroupChat = async (req, res) => {
+  const userId = req.user?.userId;
+  const participantsId = req.body?.participantsId; // participantsId is an array containing the userId of the members you wanna add .
+  const name = req.body?.name;
+  if (!name || name.trim() === '') {
+    return res
+      .status(400)
+      .json(new ApiError(400, "Name is required and can't be empty string "));
+  }
+  if (!userId) {
+    return res.status(400).json(new ApiError(400, 'User ID is required'));
+  }
+  if (!Array.isArray(participantsId) || participantsId.length < 1) {
+    return res
+      .status(400)
+      .json(
+        new ApiError(
+          400,
+          'At least 1 user IDs are required in participantsId list (As you are already included)'
+        )
+      );
+  }
+
+  try {
+    //Check all userId's provided  by the frontend are valid
+    const result = await filterValidUserIds(participantsId);
+
+    if (result.validUserIds.length == 0) {
+      return res
+        .status(400)
+        .json(new ApiError(400, "No any userId's are valid"));
+    }
+    const allParticipants = [...new Set([...result.validUserIds, userId])]; // Using Set to remove all duplicates
+
+    const chat = await Chat.create({
+      isGroup: true,
+      participants: allParticipants,
+      admins: [userId],
+      createdBy: userId,
+      name,
+    });
+
+    res
+      .status(201)
+      .json(new ApiResponse(201, chat, 'Group Chat created Successfully'));
+  } catch (error) {
+    console.error('Error  creating groupChat : ', error);
+    res
+      .status(500)
+      .json(new ApiError(500, 'internal Server Error at createGroupChat'));
+  }
+};
 
 // Get chat details
 const getChatDetails = async (req, res) => {};
