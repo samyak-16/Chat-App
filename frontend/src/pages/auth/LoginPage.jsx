ef75c7c3d28e35ee92f13d56276797654aab5a94
samyak-16 <samexecodes@gmail.com>
2025-08-16 22:10:14 +0545

    Added some  pages for auth routes and user routes

---
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,9 @@
+import LoginForm from '../../components/auth/LoginForm';
+
+const LoginPage = () => (
+  <div className="flex justify-center items-center h-screen bg-gray-100">
+    <LoginForm />
+  </div>
+);
+
+export default LoginPage;
---
+++ frontend/src/pages/auth/SignupPage.jsx
@@ -0,0 +1,9 @@
+import SignupForm from '../../components/auth/SignupForm';
+
+const SignupPage = () => (
+  <div className="flex justify-center items-center h-screen bg-gray-100">
+    <SignupForm />
+  </div>
+);
+
+export default SignupPage;
